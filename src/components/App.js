1ba8f2891c5fd5f75525f7d9e3ce6575394c036a
lily <marcela.blanco@youse.com.co>
2019-10-07 04:43:35 -0500

    modify routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import InitialPage from './user/InitialPage'
 import LoanForm from './user/LoanForm';
-import LoanResult from './user/LoanResult';
 import Header from './Header'
 
 
@@ -11,9 +10,8 @@ const App = () => (
   <div>
     <Header/>
     <BrowserRouter>
-      <Route path="/" component={InitialPage}/>
+      <Route path="/" exact component={InitialPage}/>
       <Route path="/loan_form" component={LoanForm}/>
-      <Route path="/loan_result" component={LoanResult}/>
     </BrowserRouter>
   </div>
 )
--- src/components/user/InitialPage.js
+++ src/components/user/InitialPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const InitialPage = ()=> (
-  <div>
+  <div style={{margin: "10%"}}>
     <Link to="/loan_form" className="ui button primary">New Loan Application</Link>
   </div>
 )
